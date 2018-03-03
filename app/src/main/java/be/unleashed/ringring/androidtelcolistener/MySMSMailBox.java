252cf72d54e423627a9d1650bd254bf0766cbc63
Johan Gabriels <johan.gabriels@vikingco.com>
2018-03-03 15:28:50 +0100

    Callback on log when SMS is received.

---
+++ app/src/main/java/be/unleashed/ringring/androidtelcolistener/MySMSMailBox.java
@@ -0,0 +1,54 @@
+package be.unleashed.ringring.androidtelcolistener;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+/**
+ * Created by johan on 3/03/18.
+ */
+
+public class MySMSMailBox extends BroadcastReceiver {
+    public static final String tag = "androidtelcolistener";
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        String action = intent.getAction();
+
+        // TODO: If no permission given, show an error on logcat.
+        if (action.equals("android.provider.Telephony.SMS_RECEIVED")){
+            StringBuilder sb = new StringBuilder();
+
+            final Bundle bundle = intent.getExtras();
+
+            if (bundle == null) return;
+
+            final Object[] pdus = (Object[]) bundle.get("pdus");
+            ArrayList<SmsMessage> messages = new ArrayList<SmsMessage>();
+            for (Object pdu : pdus) {
+                // Hopefully, the pdus are in the order that the parts of the SMS message should be
+                messages.add(SmsMessage.createFromPdu((byte[])pdu));
+            }
+
+            String messageBody = new String();
+            for (int index = 0; index < messages.size(); index ++) {
+                messageBody += messages.get(index).getMessageBody();
+            }
+
+            String phoneNumber = messages.get(0).getOriginatingAddress();
+            sb.append("SMS received from\t" + phoneNumber.toString());
+
+            if (messageBody != null) {
+                sb.append("\twith content:\t" + messageBody.toString());
+            }
+
+            Log.w(tag, sb.toString());
+        }
+
+    }
+}
