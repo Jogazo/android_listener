6d8696fd13b3029d776607a470bebd889812b19d
Johan Gabriels <johan.gabriels@vikingco.com>
2018-03-12 21:51:30 +0100

    Download via AndroidTelcoListener app.

--- app/src/main/java/be/unleashed/ringring/androidtelcolistener/TelcoListenerActivity.java
+++ app/src/main/java/be/unleashed/ringring/androidtelcolistener/TelcoListenerActivity.java
@@ -9,17 +9,28 @@ import android.content.Context;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class TelcoListenerActivity extends AppCompatActivity {
     public static final String tag = MySMSMailBox.tag;
     private TextView telMgrOutput;
+    private EditText getInput;
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         Log.d(tag, "TelcoListenerActivity onCreate");
         boolean close_no_permissions = false;
         super.onCreate(savedInstanceState);
+
+
         this.setContentView(R.layout.activity_telco_listener);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS)
@@ -45,6 +56,9 @@ public class TelcoListenerActivity extends AppCompatActivity {
         }
 
         this.telMgrOutput = (TextView) findViewById(R.id.telmgroutput);
+        this.getInput = (EditText) findViewById(R.id.get_input);
+
+//        String output = getHttpResponse(getInput.getText().toString());
     }
 
     @Override
@@ -86,6 +100,49 @@ public class TelcoListenerActivity extends AppCompatActivity {
 
         Log.w(tag, "callStateString: " + callStateString);
         return sb.toString();
+    }
+
+    /**
+     * Perform an HTTP GET with HttpUrlConnection.
+     *
+     * @param location
+     * @return
+     */
+    private String getHttpResponse(String location) {
+        String result = null;
+        URL url = null;
+        Log.d(tag, " " + " location = " + location);
+
+        try {
+            url = new URL(location);
+            Log.d(tag, " " + " url = " + url);
+        } catch (MalformedURLException e) {
+            Log.e(tag, " " + " " + e.getMessage());
+        }
+
+        if (url != null) {
+            try {
+                HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+                BufferedReader in = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+                String inputLine;
+
+                int lineCount = 0; // limit the lines for the example
+                while ((lineCount < 10) && ((inputLine = in.readLine()) != null)) {
+                    lineCount++;
+                    Log.v(tag, " " + " inputLine = " + inputLine);
+                    result += "\n" + inputLine;
+                }
+
+                in.close();
+                urlConn.disconnect();
+
+            } catch (IOException e) {
+                Log.e(tag, " " + " " + e.getMessage());
+            }
+        } else {
+            Log.e(tag, " " + " url NULL");
+        }
+        return result;
     }
 
 }
